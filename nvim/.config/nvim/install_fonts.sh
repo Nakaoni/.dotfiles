5ea583435e8e705a166ca01eb048342023d10556
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2024-04-05 00:20:13 +0200

    add script to install Nerdfonts

---
+++ nvim/.config/nvim/install_fonts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# @TODO: replace by a lua script
+
+NERD_FONTS_DIRECTORY="$HOME/.local/share/fonts/NerdFonts";
+FONTS="Hack.zip"
+
+if [ ! -d $NERD_FONTS_DIRECTORY ]; then
+   mkdir $NERD_FONTS_DIRECTORY
+fi
+
+cd $NERD_FONTS_DIRECTORY
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$FONTS
+unzip $FONTS
+rm $FONTS
