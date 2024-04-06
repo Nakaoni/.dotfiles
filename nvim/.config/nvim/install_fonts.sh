cbe95b875d924dfe3f13ed1168bd59bbc59a6966
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2024-04-06 12:44:26 +0200

    Add bufferline and comment

--- nvim/.config/nvim/install_fonts.sh
+++ nvim/.config/nvim/install_fonts.sh
@@ -3,13 +3,14 @@
 # @TODO: replace by a lua script
 
 NERD_FONTS_DIRECTORY="$HOME/.local/share/fonts/NerdFonts";
-FONTS="Hack.zip"
+FONTS="UbuntuMono.zip"
+VERSION="v3.2.0"
 
 if [ ! -d $NERD_FONTS_DIRECTORY ]; then
-   mkdir $NERD_FONTS_DIRECTORY
+   mkdir -p $NERD_FONTS_DIRECTORY
 fi
 
 cd $NERD_FONTS_DIRECTORY
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$FONTS
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/$VERSION/$FONTS
 unzip $FONTS
 rm $FONTS
---
+++ nvim/.config/nvim/lua/naka/plugins/comment.lua
@@ -0,0 +1,5 @@
+return {
+    "echasnovski/mini.comment",
+    version = "^v0.12",
+    opts = {}
+}
