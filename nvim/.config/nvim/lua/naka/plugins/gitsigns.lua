0e448ea5094b2495fed2eb03f968f2cfa253a39c
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2024-04-17 23:17:35 +0200

    Add gitsigns

---
+++ nvim/.config/nvim/lua/naka/plugins/gitsigns.lua
@@ -0,0 +1,9 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    version = "^0.8",
+    opts = {
+        numhl = true,
+        current_line_blame = true,
+        attach_to_untracked = true,
+    },
+}
