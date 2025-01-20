7109c439337b4d26cecd41cfb3606963e1316c0a
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2025-01-20 10:27:59 +0100

    Improve lsp

---
+++ nvim/.config/nvim/lua/naka/deps/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            {
+                "nvim-lua/plenary.nvim",
+            },
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+            },
+        },
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup({
+                extensions = {
+                    fzf = {},
+                },
+            })
+
+            telescope.load_extension("fzf")
+
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: [f]ind [f]iles" })
+            vim.keymap.set("n", "<leader>en", function()
+                builtin.find_files({
+                    cwd = vim.fn.stdpath("config")
+                })
+            end, { desc = "Telescope: [E]dit [N]eovim" })
+        end
+    }
+}
