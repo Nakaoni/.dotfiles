eb7545c00e476ebd4ee326d9098accc179fffa78
Nakaoni <16108885+Nakaoni@users.noreply.github.com>
2025-04-08 10:11:48 +0200

    Dap & phpactor & go filetype

--- nvim/.config/nvim/lua/naka/vendor/plugins/dap.lua
+++ nvim/.config/nvim/lua/naka/vendor/plugins/dap.lua
@@ -2,8 +2,28 @@ return {
     {
         "mfussenegger/nvim-dap",
         version = "^0.9",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+        },
         config = function()
             local dap = require("dap")
+            local dapui = require("dapui")
+
+            dapui.setup()
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+
             -- PHP
             dap.adapters.php = {
                 type = "executable",
